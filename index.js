de38986b1b6e9aacf5856cd7620a0cd0d4b98446
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-04-13 13:07:02 +0200

    added function changed colors

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const checkBoxContainer = document.querySelector('.checkbox-container');
 const lightSwitch = document.querySelector('.light-switch');
 const lightBulbContainer = document.querySelector('.light-bulbs');
 const lightSwitchState = document.querySelector('.light-switch-state');
+const lightBulbs = lightBulbContainer.querySelectorAll('.light-bulb');
+const intervalControl = document.querySelector('.interval-control');
+const colorWheelBtn = document.querySelector('.color-wheel');
+const colorWheels = lightBulbContainer.querySelectorAll('.color-setting');
 
 const lightSwitchLabels = {
 	on: 'off',
@@ -16,4 +20,29 @@ checkBoxContainer.addEventListener('click', (e) => {
 lightSwitch.addEventListener('change', () => {
 	lightBulbContainer.classList.toggle('on');
 	lightSwitchState.textContent = lightSwitchLabels[lightSwitchState.textContent];
+});
+
+intervalControl.addEventListener('change', (e) => {
+	const duration = e.target.value;
+	lightBulbs.forEach((lightBulb) => {
+		lightBulb.style.animationDuration = `${duration}s`;
+	});
+});
+
+lightBulbContainer.addEventListener('input', (e) => {
+	/*
+    This uses destructuring to get the themeColor
+    value set using the data attribute
+   */
+	const { themeColor } = e.target.dataset;
+	const lightBulb = e.target.parentElement;
+
+	// This updates value of the css variable to the value set on the color picker
+	lightBulb.style.setProperty(`--theme-color-${themeColor}`, e.target.value);
+});
+
+colorWheelBtn.addEventListener('click', (e) => {
+	colorWheels.forEach((colorWheel) => {
+		colorWheel.classList.toggle('hidden');
+	});
 });
